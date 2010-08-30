10e65f28bbe41c3cec94ac640d9ae85ea73babb4
JamesCook <JamesCook@localhost>
2010-08-30 12:19:45 -0400

    Added number-to-keep optional parameter, some other minor changes to code.

--- trunk/jimsbackup/backup.bat
+++ trunk/jimsbackup/backup.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-REM: Set the scriptdir variable to the directory where this script is 
-REM: located with a trailing slash
+:: Set the scriptdir variable to the directory where this script is 
+:: located with a trailing slash
 SET scriptdir=%~dp0
 
 SET rootdir=%~dp1
@@ -12,23 +12,32 @@ SET mappath=%~f2
 SET bkfolder=bk-%date:~6,4%%date:~0,2%%date:~3,2%%time:~0,2%%time:~3,2%%time:~6,2%
 
 IF [%1]==[] (
-REM: No parameters provided -- display usage.
+:: No parameters provided -- display usage.
 TYPE "%scriptdir%usage.txt"
-GOTO end
+GOTO:eof
 )
 
 DEL /F /Q "%rootdir%backup.log"
 
 IF EXIST "%rootdir%latest.txt" (
-REM: Apparently we already initialized, make hardlinks to previous 
-REM: backup.
+:: Apparently we already initialized, make hardlinks to previous 
+:: backup.
 FOR /F "usebackq delims=" %%G IN ("%rootdir%latest.txt") DO set lastbkfolder=%%G
 FOR /F "tokens=1,2 delims=|" %%G IN (%mappath%) DO (call :mk_hardlinks "%rootdir%!lastbkfolder!\%%H" "%rootdir%%bkfolder%\%%H")
 )
 
 FOR /F "tokens=1,2 delims=|" %%G IN (%mappath%) DO robocopy.exe "%%G" "%rootdir%%bkfolder%\%%H" /V /MIR>>"%rootdir%backup.log"  2>>&1
 @ECHO %bkfolder%>"%rootdir%latest.txt"
-GOTO end
+
+:: Delete old backups
+IF NOT [%3]==[] (
+:: "number-to-keep" parameter provided.  Delete oldest backups that exceed this
+:: number.
+FOR /F "skip=%3" %%G IN ('dir bk-* /A:D /B /O:-N') DO ECHO rmdir /S /Q %%G
+GOTO:eof
+)
+
+GOTO:eof
 
 :mk_hardlinks
  @ECHO MKDIR %2>>"%rootdir%backup.log"
@@ -37,6 +46,4 @@ GOTO end
  @ECHO "%scriptdir%..\bin\ln" -r %1 %2^>^>"%rootdir%backup.log">>"%rootdir%backup.log"
  "ln.exe" -r %1 %2>>"%rootdir%backup.log"
  @ECHO ln.exe Finished !date! !time!>>"%rootdir%backup.log"
- GOTO :eof
- 
-:end
+ GOTO:eof
