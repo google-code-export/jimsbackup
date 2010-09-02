2366e4a503181fb472b99e137110b5ed1884ce77
JamesCook <JamesCook@localhost>
2010-09-02 10:42:25 -0400

    backup script works with files now (just folders before).

--- trunk/jimsbackup/backup.bat
+++ trunk/jimsbackup/backup.bat
@@ -34,7 +34,8 @@ FOR /F "usebackq delims=" %%G IN ("%rootdir%latest.txt") DO set lastbkfolder=%%G
 FOR /F "tokens=1,2 delims=|" %%G IN (%mappath%) DO (call :mk_hardlinks "%rootdir%!lastbkfolder!\%%H" "%rootdir%%bkfolder%\%%H")
 )
 
-FOR /F "tokens=1,2 delims=|" %%G IN (%mappath%) DO robocopy.exe "%%G" "%rootdir%%bkfolder%\%%H" /V /MIR>>"%rootdir%backup.log"  2>>&1
+:: FOR /F "tokens=1,2 delims=|" %%G IN (%mappath%) DO robocopy.exe "%%G" "%rootdir%%bkfolder%\%%H" /V /MIR>>"%rootdir%backup.log"  2>>&1
+FOR /F "tokens=1,2 delims=|" %%G IN (%mappath%) DO (CALL :update_contents "%%G" "%rootdir%%bkfolder%\%%H")
 @ECHO %bkfolder%>"%rootdir%latest.txt"
 
 :: Delete old backups
@@ -48,10 +49,39 @@ GOTO:eof
 GOTO:eof
 
 :mk_hardlinks
- @ECHO MKDIR %2>>"%rootdir%backup.log"
- MKDIR %2>>"%rootdir%backup.log"
- @ECHO ln.exe Starting @ !date! !time!>>"%rootdir%backup.log"
- @ECHO "%scriptdir%..\bin\ln" -r %1 %2^>^>"%rootdir%backup.log">>"%rootdir%backup.log"
- ln.exe -r %1 %2>>"%rootdir%backup.log"
- @ECHO ln.exe Finished !date! !time!>>"%rootdir%backup.log"
+ IF EXIST %2\NUL (
+  :: This is a directory.  Make hard links recursively.
+  @ECHO MKDIR %2>>"%rootdir%backup.log"
+  MKDIR %2>>"%rootdir%backup.log"
+  @ECHO ln.exe Starting @ !date! !time!>>"%rootdir%backup.log"
+  @ECHO ln.exe -r %1 %2^>^>"%rootdir%backup.log">>"%rootdir%backup.log"
+  ln.exe -r %1 %2>>"%rootdir%backup.log"
+  @ECHO ln.exe Finished !date! !time!>>"%rootdir%backup.log"
+ ) ELSE (
+  :: This is a file.  Just make a hard link to this file.
+  @ECHO ln.exe Starting @ !date! !time!>>"%rootdir%backup.log"
+  @ECHO ln.exe %1 %2^>^>"%rootdir%backup.log">>"%rootdir%backup.log"
+  ln.exe %1 %2>>"%rootdir%backup.log"
+ )
  GOTO:eof
+
+:update_contents
+	IF EXIST %1\NUL (
+		:: This is a directory.
+		robocopy.exe "%1" "%2" /V /MIR>>"%rootdir%backup.log"  2>>&1
+	) ELSE (
+		:: This is a file.
+		IF EXIST "%2" (
+			:: File exists in destination.
+			IF NOT (%~z1) == (%~z2) (
+				:: File sizes are not equal.  Force copy of source to dest.
+				xcopy "%1" "%2" /V /H /R /K /Y /Z>>"%rootdir%backup.log"  2>>&1
+			) ELSE (
+				:: File sizes are equal.  Copy if source is newer.
+				xcopy "%1" "%2" /D /V /H /R /K /Y /Z>>"%rootdir%backup.log"  2>>&1
+			)
+		) ELSE (
+			:: File does not exist in destination.
+			xcopy "%1" "%2" /V /H /R /K /Y /Z>>"%rootdir%backup.log"  2>>&1
+		)
+	)
