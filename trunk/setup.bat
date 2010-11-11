9562b38a0cd644bbe49c46cfb855c4d19209a934
JamesCook <JamesCook@localhost>
2010-11-11 13:07:04 -0500

    Made some changes to the distribution (listed in CHANGES.TXT).  Bumped version number to 0.5c3

--- trunk/setup.bat
+++ trunk/setup.bat
@@ -6,11 +6,11 @@ REM del /f /q .\docs\readme.html
 rmdir /S /Q build
 rmdir /S /Q dist
 mkdir build
-mkdir build\jimsbackup-%version%
-copy .\VERSION.TXT .\build\jimsbackup-%version%\
-xcopy /E /I jimsbackup build\jimsbackup-%version%\jimsbackup /EXCLUDE:.\setup.excludes
-mkdir build\jimsbackup-%version%\docs
-rst2html.py readme.txt .\build\jimsbackup-%version%\docs\readme.html
+mkdir build\jimsbackup
+copy .\VERSION.TXT .\build\jimsbackup\
+xcopy /E /I jimsbackup build\jimsbackup\scripts /EXCLUDE:.\setup.excludes
+mkdir build\jimsbackup\docs
+rst2html.py readme.txt .\build\jimsbackup\docs\readme.html
 cd build
 "C:\program files\7-zip\7z" a -tzip ..\dist\jimsbackup-%version%.zip -i!jimsbackup*
 cd ..
